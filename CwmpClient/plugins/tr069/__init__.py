9247dd0da09a63885f4d3c397717f1c7a96d2019
Rémi BONNET <bonnet@fanaticalhelp.com>
2024-04-02 18:03:06 +0200

    Stun authentication

--- CwmpClient/plugins/tr069/__init__.py
+++ CwmpClient/plugins/tr069/__init__.py
@@ -1,16 +1,20 @@
 
-import logging, random, socket
+import logging
+import random
+import socket
 from CwmpClient.nodes import *
 from asyncio import sleep
 from .session import Tr69Inform
 from .stunudp import STUN_client
 log = logging.getLogger("TR-069")
 
+
 async def loader(rootnode):
     log.debug('Load defaults parameters')
     config = ConfigFileParameter(rootnode, "tr069.ini")
     config.addItem('Device.ManagementServer.EnableCWMP', True)
-    config.addItem('Device.ManagementServer.ConnectionRequestURL', str, writable=False)
+    config.addItem('Device.ManagementServer.ConnectionRequestURL',
+                   str, writable=False)
     config.addItem('Device.ManagementServer.ConnectionRequestPassword', str)
     config.addItem('Device.ManagementServer.ConnectionRequestUsername', str)
     config.addItem('Device.ManagementServer.URL', "http://acs.dites.team/")
@@ -18,22 +22,32 @@ async def loader(rootnode):
     config.addItem('Device.ManagementServer.Username', str)
     config.addItem('Device.ManagementServer.PeriodicInformEnable', False)
     config.addItem('Device.ManagementServer.PeriodicInformInterval', 300)
-    config.addItem('Device.DeviceInfo.Manufacturer', "Dites Telecom", writable=False)
-    config.addItem('Device.DeviceInfo.ManufacturerOUI', "FFFFFF", writable=False)
+    config.addItem('Device.DeviceInfo.Manufacturer',
+                   "Dites Telecom", writable=False)
+    config.addItem('Device.DeviceInfo.ManufacturerOUI',
+                   "FFFFFF", writable=False)
     config.addItem('Device.DeviceInfo.ProductClass', "DTBox1", writable=False)
     config.addItem('Device.DeviceInfo.SerialNumber', "0123456", writable=False)
     config.addItem('Device.DeviceInfo.ProvisioningCode', "")
-    config.addItem('Device.DeviceInfo.SoftwareVersion', "0.0.1", writable=False)
-    config.addItem('Device.DeviceInfo.HardwareVersion', "1.0.0", writable=False)
+    config.addItem('Device.DeviceInfo.SoftwareVersion',
+                   "0.0.1", writable=False)
+    config.addItem('Device.DeviceInfo.HardwareVersion',
+                   "1.0.0", writable=False)
     config.addItem('Device.DeviceInfo.ParameterKey', "")
     config.addItem('Device.RootDataModelVersion', "2.15", writable=False)
 
     config.addItem('Device.ManagementServer.STUNEnable', True)
-    config.addItem('Device.ManagementServer.NATDetected', False, writable=False)
-    config.addItem('Device.ManagementServer.STUNServerAddress', "stun.l.google.com")
+    config.addItem('Device.ManagementServer.NATDetected',
+                   False, writable=False)
+    config.addItem('Device.ManagementServer.STUNServerAddress',
+                   "stun.l.google.com")
+    config.addItem('Device.ManagementServer.STUNUsername', "")
+    config.addItem('Device.ManagementServer.STUNPassword', "")
     config.addItem('Device.ManagementServer.STUNServerPort', 19302)
-    config.addItem('Device.ManagementServer.UDPConnectionRequestAddress', "", writable=False)
+    config.addItem(
+        'Device.ManagementServer.UDPConnectionRequestAddress', "", writable=False)
     config.write()
+
 
 async def start(app):
     """
@@ -44,12 +58,13 @@ async def start(app):
     # First start STUN task
     stun = STUN_client(app)
     await stun.load()
-    
+
     app.root['Device']['ManagementServer']['ConnectionRequestURL']('http://%s:%d/%s' % (
         socket.gethostbyname_ex(socket.gethostname())[2][0],
         7547,
-        "".join([random.choice("acbdefghijhlmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXIZ0123456789") for i in range(32)])
-        ))
+        "".join([random.choice(
+            "acbdefghijhlmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXIZ0123456789") for i in range(32)])
+    ))
 
     events = ['1 BOOT']
     if len(sys.argv) > 1:
--- CwmpClient/plugins/tr069/stunudp.py
+++ CwmpClient/plugins/tr069/stunudp.py
@@ -1,18 +1,50 @@
-from CwmpClient.nodes import ConfigFileParameter
 import asyncio
 import logging
 import binascii
 import struct
 import random
 import socket
+import hashlib
+import hmac
 import typing
 import enum
 from CwmpClient.app import App
 
 log = logging.getLogger()
 
-StunResponse = typing.NewType(
-    'StunResponse', typing.Dict[str, typing.Tuple[str, int]])
+
+class STUN_MSG_TYPE(enum.Enum):
+    REQ_BIND = b'\x00\x01'
+    REQ_ALLOCATE = b'\x00\x03'
+
+
+class STUN_RES_TYPE(enum.Enum):
+    RES_ALLOCATE_ERROR = b'\x01\x13'
+    RES_ALLOCATE_SUCESS = b'\x01\x03'
+
+
+class STUN_MSG_ATTR(enum.Enum):
+    ATTR_MAPPED_ADDR = b'\x00\01'
+    ATTR_SOURCE_ADDR = b'\x00\x04'
+    ATTR_CHANGE_ADDR = b'\x00\x05'
+    ATTR_USERNAME = b'\x00\x06'
+    ATTR_MSG_INTEGRITY = b'\x00\x08'
+    ATTR_ERROR_CODE = b'\x00\x09'
+    ATTR_CHANNEL_NUMBER = b'\x00\x0C'
+    ATTR_LIFETIME = b'\x00\x0D'
+    ATTR_XOR_PEER_ADDR = b'\x00\x12'
+    ATTR_DATA = b'\x00\x13'
+    ATTR_REALM = b'\x00\x14'
+    ATTR_NONCE = b'\x00\x15'
+    ATTR_XOR_RELAYED_ADDR = b'\x00\x16'
+    ATTR_REQ_FAMILY = b'\x00\x17'
+    ATTR_EVEN_PORT = b'\x00\x18'
+    ATTR_REQ_TRANSPORT = b'\x00\x19'
+    ATTR_DONT_FRAGMENT = b'\x00\x1A'
+    ATTR_XOR_MAPPED_ADDR = b'\x00\x20'
+    ATTR_RESERVATION_TOKEN = b'\x00\x22'
+
+    ATTR_SOFTWARE = b'\x80\x22'
 
 
 @enum.unique
@@ -59,65 +91,174 @@ class STUN_CODE:
     ATTR_RESERVATION_TOKEN = b'\x00\x22'
 
 
+class StunResponse:
+    def __init__(self, transid) -> None:
+        self.transid = transid
+        self.attr: typing.Dict[STUN_MSG_ATTR, any] = dict()
+
+    def set(self, attr: STUN_MSG_ATTR, value: bytes):
+        self.attr[attr] = value
+
+    def getXorAddress(self, attr: STUN_MSG_ATTR):
+        data = self.attr[attr]
+        family, = struct.unpack_from('c', data, 1)
+        port, = struct.unpack_from('>h', data ^ self.transid[0:2], 2)
+        ip = data[4:8] ^ self.transid[0:4]
+
+        log.debug('Family: %d, X-Port: %d, X-Addr: %s', family,
+                  port, ip.hex())
+
+    @staticmethod
+    def frombytes(data) -> 'StunResponse':
+        if data[4:8] != b'\x21\x12\xa4\x42':
+            raise Exception('Bad magic cookie %s', data[4:8].hex())
+
+        transid = data[8:20]
+        is_error = False
+        log.debug('Get transId %s', transid.hex())
+        if data[0:2] == STUN_RES_TYPE.RES_ALLOCATE_ERROR.value:
+            res = StunAllocationError(transid)
+            is_error = True
+        elif data[0:2] == STUN_RES_TYPE.RES_ALLOCATE_SUCESS.value:
+            res = StunResponse(transid)
+        else:
+            raise NotImplementedError(
+                'Response type %s not implemanted', data[0:2].hex())
+
+        msg_len, = struct.unpack_from('>h', data, 2)
+        print(msg_len)
+        pos_reader = 20
+        while pos_reader < msg_len:
+            attr_len, = struct.unpack_from('>h', data, pos_reader+2)
+            try:
+                attr = STUN_MSG_ATTR(data[pos_reader:pos_reader+2])
+                log.debug('Attr %s (len %d) = %s', attr, attr_len,
+                          data[pos_reader+4:pos_reader+4+attr_len])
+                res.set(attr, data[pos_reader+4:pos_reader+4+attr_len])
+            except ValueError:
+                log.warn('Unknow attribute %s',
+                         data[pos_reader:pos_reader+2].hex())
+            pos_reader += 4 + attr_len + (attr_len % 4)
+
+        if is_error:
+            raise res
+        return res
+
+
+class StunAllocationError(StunResponse, Exception):
+    pass
+
+
 class StunMessage:
     MAGIC_COOKIE = b'\x21\x12\xa4\x42'
 
-    class MSG_TYPE(enum.Enum):
-        REQ_BIND = b'\x00\x01'
-        REQ_ALLOCATE = b'\x00\x03'
-
-    class MSG_ATTR(enum.Enum):
-        ATTR_MAPPED_ADDR = b'\x00\01'
-        ATTR_SOURCE_ADDR = b'\x00\x04'
-        ATTR_CHANGE_ADDR = b'\x00\x05'
-        ATTR_CHANNEL_NUMBER = b'\x00\x0C'
-        ATTR_LIFETIME = b'\x00\x0C'
-        ATTR_XOR_PEER_ADDR = b'\x00\x12'
-        ATTR_DATA = b'\x00\x13'
-        ATTR_XOR_RELAYED_ADDR = b'\x00\x16'
-        ATTR_EVEN_PORT = b'\x00\x18'
-        ATTR_REQ_TRANSPORT = b'\x00\x19'
-        ATTR_DONT_FRAGMENT = b'\x00\x1A'
-        ATTR_RESERVATION_TOKEN = b'\x00\x22'
-
-    def __init__(self, type: MSG_TYPE, clientHandler: 'STUN_client' = None) -> None:
+    def __init__(self, type: STUN_MSG_TYPE, clientHandler: 'STUN_client' = None) -> None:
         self.type = type
         self.tranid = random.randbytes(12)
-        self.attrs: typing.Dict[self.MSG_ATTR, bytes] = dict()
+        self.attrs: typing.Dict[STUN_MSG_ATTR, bytes] = dict()
         self.client = clientHandler
 
-    def set(self, attr: MSG_ATTR, *value: any):
-        if attr == self.MSG_ATTR.ATTR_DONT_FRAGMENT:
+    def copy(self) -> 'StunMessage':
+        newobj = StunMessage(self.type, self.client)
+        newobj.attrs = self.attrs.copy()
+        return newobj
+
+    def set(self, attr: STUN_MSG_ATTR, *value: any):
+        if attr == STUN_MSG_ATTR.ATTR_DONT_FRAGMENT:
             if len(value) != 0:
                 raise Exception('Bad number of arguments')
             self.attrs[attr] = b''
+        elif attr == STUN_MSG_ATTR.ATTR_REQ_TRANSPORT:
+            if len(value) != 1:
+                raise Exception('Bad number of arguments')
+            self.attrs[attr] = struct.pack('b', value[0]) + b'\x00\x00\x00'
+        elif attr == STUN_MSG_ATTR.ATTR_MSG_INTEGRITY:
+            if len(value) == 0:
+                self.attrs[attr] = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+        elif attr == STUN_MSG_ATTR.ATTR_USERNAME:
+            if len(value) == 0:
+                raise Exception('Bad number of arguments')
+            self.attrs[attr] = str.encode(value[0])
+        else:
+            if len(value) != 1:
+                raise Exception('Bad number of arguments')
+            self.attrs[attr] = value[0]
 
     def data(self) -> bytes:
-        data = bytearray()
+        data = bytearray(b''.join([self.type.value, b'\x00\x00',
+                         self.MAGIC_COOKIE, self.tranid]))
         msg_len = 0
+
         for attr_type, attr_content in self.attrs.items():
+            log.debug('Pack %s = %s', attr_type, attr_content)
             data.extend(attr_type.value)
-            data.extend(struct.pack('h', len(attr_content)))
+            data.extend(struct.pack('>h', len(attr_content)))
             data.extend(attr_content)
+            trailling = len(attr_content) % 4
+            if trailling != 0:
+                for _ in range(4 - trailling):
+                    data.extend(b'\x00')
+
+            if attr_type == STUN_MSG_ATTR.ATTR_MSG_INTEGRITY:
+                struct.pack_into('>h', data, 2, len(data) - 20)
+                key = b''.join([str.encode(self.client.user), b':',
+                                self.attrs[STUN_MSG_ATTR.ATTR_REALM], b':', str.encode(self.client.password)])
+                md5_key = hashlib.md5(key)
+                integrity = hmac.HMAC(
+                    md5_key.digest(), data[0:-24], hashlib.sha1)
+                data[len(data) - 20:] = integrity.digest()
+
             log.debug('Attr Type: %s, %d, %s', attr_type.value.hex(), len(
                 attr_content), attr_content.hex())
             msg_len += len(attr_content) + 4
 
+        struct.pack_into('>h', data, 2, len(data) - 20)
+
         log.debug('Len: %s, Dat: %s', msg_len,
                   struct.pack('>h', msg_len).hex())
 
-        return b''.join([self.type.value, struct.pack('>h', msg_len), self.MAGIC_COOKIE, self.tranid, data])
+        return data
 
     def __repr__(self) -> str:
         return ''.join(['StunMessage[', self.data().hex(), ']'])
 
-    async def sendto(self, stunClient: 'STUN_client' = None, addr=None):
+    async def sendto(self, stunClient: 'STUN_client' = None, addr=None) -> StunResponse:
         if stunClient is None:
             if self.client is None:
                 raise AssertionError("Client not defined")
             stunClient = self.client
-        stunClient.transport.sendto(
-            self.data(), addr if addr is not None else stunClient.sturn_addr)
+        stunClient.results[self.tranid] = asyncio.get_event_loop(
+        ).create_future()
+
+        try:
+            log.debug('Try send to : %s',
+                      addr if addr is not None else stunClient.sturn_addr)
+            for _ in range(5):
+                stunClient.transport.sendto(
+                    self.data(), addr if addr is not None else stunClient.sturn_addr)
+                await asyncio.wait([stunClient.results[self.tranid]], timeout=1)
+                if stunClient.results[self.tranid].done():
+                    return stunClient.results[self.tranid].result()
+
+            raise ConnectionRefusedError()
+        except StunAllocationError as err:
+            if err.attr[STUN_MSG_ATTR.ATTR_ERROR_CODE][2:4] == b'\x04\x01' and STUN_MSG_ATTR.ATTR_MSG_INTEGRITY not in self.attrs:
+                log.debug('Authentication needed')
+                auth_msg = self.copy()
+                auth_msg.set(STUN_MSG_ATTR.ATTR_USERNAME, stunClient.user)
+                auth_msg.set(STUN_MSG_ATTR.ATTR_REALM,
+                             err.attr[STUN_MSG_ATTR.ATTR_REALM])
+                auth_msg.set(STUN_MSG_ATTR.ATTR_NONCE,
+                             err.attr[STUN_MSG_ATTR.ATTR_NONCE])
+                auth_msg.set(STUN_MSG_ATTR.ATTR_MSG_INTEGRITY)
+                return await auth_msg.sendto(stunClient, addr)
+
+            else:
+                log.error('Unknown error code: %s',
+                          err.attr[STUN_MSG_ATTR.ATTR_ERROR_CODE][2:4].hex())
+                raise err
+        finally:
+            del stunClient.results[self.tranid]
 
 
 def b2a_hexstr(abytes):
@@ -134,11 +275,18 @@ class StunClientProtocol(asyncio.DatagramProtocol):
     def __init__(self, client: 'STUN_client') -> None:
         super().__init__()
         self.client = client
-        self.handler: typing.List[callable[bytes]] = []
 
     def datagram_received(self, data, addr):
         log.info("Receiving UDP %s:%d = %s",
                  addr[0], addr[1], b2a_hexstr(data))
+
+        try:
+            res = StunResponse.frombytes(data)
+            self.client.results[res.transid].set_result(res)
+        except StunAllocationError as err:
+            self.client.results[err.transid].set_exception(err)
+        return
+
         log.debug('Infos from %s', addr)
         result: StunResponse = {'source_addr': (
             None, None), 'remote_addr': (None, None), 'change_addr': (None, None)}
@@ -204,6 +352,9 @@ class STUN_client:
         self.keepalive: asyncio.Task = None
         self._last_response: asyncio.Future = None
         self.sturn_addr: typing.Tuple[str, int] = None
+        self.results: typing.Dict[bytes, StunResponse] = dict()
+        self.user: str = None
+        self.password: str = None
 
     async def _send_stun_request(self, remote_addr: typing.Tuple[str, int] = None, send_data=b'', req_type=STUN_CODE.REQ_BIND):
         if remote_addr is None:
@@ -265,18 +416,25 @@ class STUN_client:
             await self._get_public_addr()
 
     async def _start_turn(self):
-        allocation = StunMessage(StunMessage.MSG_TYPE.REQ_ALLOCATE)
-        allocation.set(StunMessage.MSG_ATTR.ATTR_DONT_FRAGMENT)
-        allocation.set(StunMessage.MSG_ATTR.ATTR_REQ_TRANSPORT,
+        allocation = StunMessage(STUN_MSG_TYPE.REQ_ALLOCATE, self)
+        allocation.set(STUN_MSG_ATTR.ATTR_REQ_TRANSPORT,
                        socket.IPPROTO_UDP)
+        allocation.set(STUN_MSG_ATTR.ATTR_LIFETIME, b'\x00\x00\x03\x09')
+        allocation.set(STUN_MSG_ATTR.ATTR_EVEN_PORT, b'\x80')
+        allocation.set(STUN_MSG_ATTR.ATTR_REQ_FAMILY, b'\x01\x00\x00\x00')
+        allocation.set(STUN_MSG_ATTR.ATTR_DONT_FRAGMENT)
         print(allocation)
-        await allocation.sendto(self.transport, self.sturn_addr)
+        return await allocation.sendto(self)
 
     async def load(self):
         loop = asyncio.get_event_loop()
         server = self.app.root['Device']['ManagementServer']['STUNServerAddress'](
         )
         port = self.app.root['Device']['ManagementServer']['STUNServerPort'](
+        )
+        self.user = self.app.root['Device']['ManagementServer']['STUNUsername'](
+        )
+        self.password = self.app.root['Device']['ManagementServer']['STUNPassword'](
         )
         sturn_addr_info = await loop.getaddrinfo(server, port, proto=socket.IPPROTO_UDP, family=socket.AF_INET)
         print(sturn_addr_info)
@@ -294,7 +452,9 @@ class STUN_client:
         # print(nat_type)
 
         # Require TURN protocol
-        await self._start_turn()
+        mapped = await self._start_turn()
+        log.info('Public IP: %s, Relai IP: %s', mapped.getXorAddress(
+            STUN_MSG_ATTR.ATTR_XOR_MAPPED_ADDR), mapped.getXorAddress(STUN_MSG_ATTR.ATTR_XOR_RELAYED_ADDR))
         self.keepalive = asyncio.create_task(self._keepalive())
         await asyncio.sleep(100)
 
